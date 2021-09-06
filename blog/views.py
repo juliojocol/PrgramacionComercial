1c7624d1492afd2f2b53a6a128df9dd6427fd97e
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-09-06 15:33:21 -0600

    Vista de ccs usando css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.utils import timezone
 from django.utils.regex_helper import normalize
 
@@ -8,3 +8,6 @@ def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/publicacion_lista.html', {'publicaciones':publicaciones})
 
+def publicacion_detalle(request, pk):
+    publicacion = get_list_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/publicacion_detalle.html', {'publicacion':publicacion})
