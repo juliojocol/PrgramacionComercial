1280685d20c2010c687d084efe021972b2dfe35c
Julio Jocol <juliojocol12@mesoamericana.edu.gt>
2021-09-09 16:25:05 -0600

    Pestañas de añadir y modificar

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.publicacion_lista, name='publicacion_lista'),
     path('publicacion/<int:pk>/', views.publicacion_detalle, name='publicacion_detalle'),
+    path('publicacio/nueva/', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar')
 ]
